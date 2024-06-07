c5c1f082db0b108fd8d351c6ea8caba06d2b79d9
gopalbbharadva <gopalbbharadva@gmail.com>
2024-06-07 10:02:16 +0530

    initial repo setup

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    colors: {
+      marineBlue: "hsl(213, 96%, 18%)",
+      purplishBlue: "hsl(243, 100%, 62%)",
+      astelBlue: "hsl(228, 100%, 84%)",
+      lightBlue: "hsl(206, 94%, 87%)",
+      strawberryRed: "hsl(354, 84%, 57%)",
+      coolGray: "hsl(231, 11%, 63%)",
+      lightGray: "hsl(229, 24%, 87%)",
+      magnolia: "hsl(217, 100%, 97%)",
+      alabaster: "hsl(231, 100%, 99%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
